112f3a23692abb16a339ace13d43b04ba8d287c2
yana1291 <yana.shalin@mail.ru>
2025-04-26 16:51:03 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,84 +1,43 @@
-import os
-import sqlite3
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils import executor
-from aiogram.dispatcher.filters import CommandStart
 
-TOKEN = os.getenv("BOT_TOKEN")
-bot = Bot(token=TOKEN)
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = 'YOUR_TELEGRAM_BOT_API_TOKEN'
+CHANNEL_ID = '@YOUR_CHANNEL_USERNAME'
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-# База данных
-conn = sqlite3.connect("referral_bot.db")
-cursor = conn.cursor()
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY,
-    invited_by INTEGER,
-    balance INTEGER DEFAULT 0
-)
-""")
-conn.commit()
+user_data = {}
 
-CHANNEL_ID = '@QE126T'  # Сюда вставь свой канал
-
-# Старт с реферальной ссылкой
-@dp.message_handler(CommandStart(deep_link=True))
-async def start_with_ref(message: types.Message):
-    ref_id = message.get_args()
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    args = message.get_args()
+    referrer_id = int(args) if args.isdigit() else None
     user_id = message.from_user.id
 
-    cursor.execute("SELECT * FROM users WHERE user_id=?", (user_id,))
-    if cursor.fetchone() is None:
-        markup = InlineKeyboardMarkup()
-        markup.add(InlineKeyboardButton("✅ Подписался", callback_data=f"checksub:{ref_id}"))
-        await message.answer(
-            "Добро пожаловать! Получи свою реферальную ссылку, приглашай друзей и получай Голду!\n\n"
-            "Для начала подпишитесь на канал: https://t.me/QE126T",
-            reply_markup=markup
-        )
-    else:
-        await message.answer("Вы уже зарегистрированы!")
+    if user_id not in user_data:
+        user_data[user_id] = {'balance': 0, 'referrals': [], 'referrer': referrer_id}
 
-@dp.message_handler(commands=["start"])
-async def start(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button = types.KeyboardButton("Подписался")
+    keyboard.add(button)
+
+    await message.answer(
+        "Добро пожаловать! Получи свою реферальную ссылку, приглашай друзей и получай Голду!
+"
+        "Для начала проверь подписку на канал: https://t.me/QE126T",
+        reply_markup=keyboard
+    )
+
+@dp.message_handler(lambda message: message.text == "Подписался")
+async def check_subscription(message: types.Message):
     user_id = message.from_user.id
-
-    cursor.execute("SELECT * FROM users WHERE user_id=?", (user_id,))
-    if cursor.fetchone() is None:
-        markup = InlineKeyboardMarkup()
-        markup.add(InlineKeyboardButton("✅ Подписался", callback_data="checksub:0"))
-        await message.answer(
-            "Добро пожаловать! Получи свою реферальную ссылку, приглашай друзей и получай Голду!\n\n"
-            "Для начала подпишитесь на канал: https://t.me/QE126T",
-            reply_markup=markup
-        )
-    else:
-        await message.answer("Вы уже зарегистрированы!")
-
-# Проверка подписки
-@dp.callback_query_handler(lambda c: c.data and c.data.startswith('checksub'))
-async def check_subscription(callback_query: types.CallbackQuery):
-    ref_id = int(callback_query.data.split(":")[1])
-    user_id = callback_query.from_user.id
-
-    member = await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=user_id)
-    if member.status in ['member', 'administrator', 'creator']:
-        cursor.execute("SELECT * FROM users WHERE user_id=?", (user_id,))
-        if cursor.fetchone() is None:
-            cursor.execute("INSERT INTO users (user_id, invited_by, balance) VALUES (?, ?, ?)", (user_id, ref_id, 0))
-            conn.commit()
-            if ref_id != 0:
-                cursor.execute("UPDATE users SET balance = balance + 10 WHERE user_id=?", (ref_id,))
-                conn.commit()
-                await bot.send_message(ref_id,
-                    "Поздравляем! Новый пользователь зарегистрировался по вашей реферальной ссылке!\n"
-                    "Сделайте скриншот этого сообщения и отправьте его в канал @QE126T чтобы получить 0.5 голды!"
-                )
-
-        bot_user = await bot.get_me()
-        link = f"https://t.me/{bot_user.username}?start={user_id}"
+    chat_member = await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=user_id)
+    if chat_member.status in ["member", "creator", "administrator"]:
+        link = f"https://t.me/YOUR_BOT_USERNAME?start={user_id}"
         await bot.send_message(user_id,
             f"Вы успешно подписались!
 "
@@ -87,8 +46,22 @@ async def check_subscription(callback_query: types.CallbackQuery):
 "
             f"Приглашайте друзей и получайте бонусы!"
         )
+        referrer_id = user_data.get(user_id, {}).get('referrer')
+        if referrer_id and referrer_id in user_data:
+            user_data[referrer_id]['balance'] += 0.5
+            await bot.send_message(referrer_id,
+                "Поздравляем! Новый пользователь зарегистрировался по вашей ссылке!
+"
+                "Сделайте скриншот этого сообщения и отправьте его в канал @QE126T для получения 0.5 Голды."
+            )
     else:
-        await bot.send_message(user_id, "❗ Вы не подписаны на канал! Пожалуйста, подпишитесь.")
+        await message.answer("Вы ещё не подписались на канал!")
 
-if __name__ == "__main__":
-    executor.start_polling(dp)
+@dp.message_handler(commands=['balance'])
+async def check_balance(message: types.Message):
+    user_id = message.from_user.id
+    balance = user_data.get(user_id, {}).get('balance', 0)
+    await message.answer(f"Ваш баланс: {balance} Голды.")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
